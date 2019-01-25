80cc17f063d4e7348ba9de89b4e4dc4814379655
NichSez <hrlayeghi@gmail.com>
2019-01-25 12:34:15 +0330

    Admin login finish and completed

--- app/src/main/java/com/example/nich_sez/gymadmin/LoginActivity.java
+++ app/src/main/java/com/example/nich_sez/gymadmin/LoginActivity.java
@@ -1,29 +1,132 @@
 package com.example.nich_sez.gymadmin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText UserTxt ;
+    Button LoginBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        if (SharedPrefManager.getInstance(this).isLoggedIn()) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+        UserTxt = (EditText) findViewById(R.id.editTextNationalCode);
+        LoginBtn = (Button) findViewById(R.id.buttonLogin);
+
+
+        //User login
+        findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                adminLogin();
             }
         });
+    }
+
+    private void adminLogin() {
+        //first getting the values
+        final String nationalCode = UserTxt.getText().toString();
+
+
+        //validating inputs
+        if (TextUtils.isEmpty(nationalCode)) {
+            UserTxt.setError("لطفا کد ملی خود را وارد کنید:)");
+            return;
+        }
+
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String baseUrl = "https://sayehparsaei.com/GymAPI/";
+        String file = "Admin";
+        String uri = baseUrl + file;
+
+
+        //if everything is fine
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, uri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("response",String.valueOf(response));
+                        try {
+                            //converting response to json object
+                            JSONObject obj = new JSONObject(response);
+
+                            //if no error in response
+                            if (obj.getInt("Error_Code")==200) {
+
+                                //creating a new user object
+                                Admin admin = new Admin(
+                                        obj.getString("User_ID"),
+                                        obj.getString("Name"),
+                                        obj.getString("Family"),
+                                        obj.getInt("Position"),
+                                        obj.getString("Token")
+                                );
+
+                                //storing the user in shared preferences
+                                SharedPrefManager.getInstance(getApplicationContext()).userLogin(admin);
+
+                                //starting the profile activity
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("National_Code", nationalCode);
+                return params;
+            }
+        };
+
+        queue.add(stringRequest);
     }
 
 }
--- app/src/main/java/com/example/nich_sez/gymadmin/SharedPrefManager.java
+++ app/src/main/java/com/example/nich_sez/gymadmin/SharedPrefManager.java
@@ -1,5 +1,92 @@
 package com.example.nich_sez.gymadmin;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+
+//here for this class we are using a singleton pattern
+
 public class SharedPrefManager {
 
+    //the constants
+    private static final String SHARED_PREF_NAME = "simplifiedcodingsharedpref";
+    private static final String KEY_ID = "keyid";
+    private static final String KEY_NAME = "keyname";
+    private static final String KEY_FAMILY = "keyfamily";
+    private static final String KEY_POSITION = "keyposition";
+    private static final String KEY_TOKEN ="keytoken";
+
+
+    private static SharedPrefManager mInstance;
+    private static Context mCtx;
+
+    private SharedPrefManager(Context context) {
+        mCtx = context;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(context);
+        }
+        return mInstance;
+    }
+
+    //method to let the user login
+    //this method will store the user data in shared preferences
+    public void userLogin(Admin admin) {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_ID, admin.getAdminId());
+        editor.putString(KEY_NAME, admin.getAdminName());
+        editor.putString(KEY_FAMILY, admin.getAdminFamily());
+        editor.putInt(KEY_POSITION, admin.getAdminPosition());
+        editor.putString(KEY_TOKEN, admin.getToken());
+        editor.apply();
+    }
+
+    //this method will checker whether user is already logged in or not
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        getUser();
+        return sharedPreferences.getString(KEY_NAME, null) != null;
+    }
+
+    //this method will give the logged in user
+    public Admin getUser() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return new Admin(
+                sharedPreferences.getString(KEY_ID, null),
+                sharedPreferences.getString(KEY_NAME, null),
+                sharedPreferences.getString(KEY_FAMILY, null),
+                sharedPreferences.getInt(KEY_POSITION, 0),
+                sharedPreferences.getString(KEY_TOKEN, null)
+        );
+    }
+
+    //this method will logout the user
+    public void logout(Context context) {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        ((Activity)context).finish();//Block back button to MainActivity
+        mCtx.startActivity(new Intent(mCtx, LoginActivity.class));
+    }
+
+
+    //this method will keep user logged in
+    public boolean stillLogIn(){
+        if(Admin.token==null){
+            SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+            mCtx.startActivity(new Intent(mCtx, LoginActivity.class));
+            return false;
+        }
+        return true;
+
+    }
 }
